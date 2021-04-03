8eb1f260635988ce74ce3099464ca59850e3cc6c
unknown <uvsgwi@gmail.com>
2021-04-03 18:46:12 -0400

    v1.3 - 2 functions

--- P4/P4.py
+++ P4/P4.py
@@ -1,6 +1,52 @@
 def mat_to_str(mat):
+    result=''
+
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            result+=str(mat[i][j])  + ' '
+        result+='\n'
     
-# def minesweeper(mat):
+    return result
+
+def minesweeper(mat):
+    copy=mat[:]
+
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            ct=0
+            if mat[i][j]=='x':
+                for k in range(len(mat)):
+                    for l in range(len(mat[k])):
+                        if (mat[k][l]=='m') and (l==j or l==j+1 or l==j-1) and (i==k or i==k+1 or i==k-1):
+                            ct+=1
+                mat[i][j]=ct
+    copy=[]
+
+
+
+
+
+
+
+
+
+
+
+
+    # mines, next_spot=0,[]
+
+    # for i in range(len(mat)):
+    #     for j in range(len(mat[i])):
+    #         if mat[i][j]=='m':
+    #             mines+=1
+    # i,j=0,0
+    # for i in range(len(mat)):
+    #     for j in range(len(mat[i])):
+    #         if mat[i][j]=='x':
+    #             mat[i][j]=mines
+        
+
+                
     
 # def transpose(mat):
 

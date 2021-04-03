a6520fb476a0e23d7e25be1b7b0f066dee5711c6
unknown <uvsgwi@gmail.com>
2021-04-03 12:10:31 -0400

    v1.2

--- P4/P4.py
+++ P4/P4.py
@@ -1,6 +1,11 @@
 def mat_to_str(mat):
-def minesweeper(mat):
-def transpose(mat):
-def mat_mul_ele(mat1, mat2):
-def mat_mul_row_col(mat1, mat2, i, j):
-def mat_mul_mat(mat1, mat2):
+    
+# def minesweeper(mat):
+    
+# def transpose(mat):
+
+# def mat_mul_ele(mat1, mat2):
+
+# def mat_mul_row_col(mat1, mat2, i, j):
+
+# def mat_mul_mat(mat1, mat2):
